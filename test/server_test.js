ef74b105c7dd54cd89019024775603f66d2392c6
Yueyue07 <xww.qyy@gmail.com>
2016-01-18 13:25:29 -0800

    simplified server test with response function

--- test/server_test.js
+++ test/server_test.js
@@ -2,17 +2,17 @@ const Router = require(__dirname + '/../lib/router');
 const http = require('http');
 const server = require(__dirname + '/../lib/server').server;
 const response = require(__dirname + '/../lib/response').buildResponse;
-
 var router = new Router();
-router.get('/test', response('test from get'));
 
-router.put('/test', response('test from put'));
+router.get('/test', response({msg: 'test from get'}));
 
-router.post('/test', response('test from post'));
+router.put('/test', response({msg: 'test from put'}));
 
-router.patch('/test', response('test from patch'));
+router.post('/test', response({msg: 'test from post'}));
 
-router.delete('/test', response('test from delete'));
+router.patch('/test', response({msg: 'test from patch'}));
+
+router.delete('/test', response({msg: 'test from delete'}));
 
 module.exports = server(router.route()).listen(4000, () => {
   console.log('server up');
